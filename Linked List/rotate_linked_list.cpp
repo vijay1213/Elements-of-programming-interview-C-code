3ad74d15ef419af36429cc849ad8be4ce488e231
Vijay Chathe <76934330+vijay1213@users.noreply.github.com>
2022-07-26 16:12:36 +0530

    Create rotate_linked_list.cpp

---
+++ Linked List/rotate_linked_list.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head) return head;
+        
+        int len=1; // number of nodes
+        ListNode *newH, *tail;
+        newH=tail=head;
+        
+        while(tail->next)  // get the number of nodes in the list
+        {
+         
+            len++;
+               tail = tail->next;
+          
+        }
+     //   len = 5;
+        //  cout << len << endl;
+       tail->next = head; // circle the link // it is not pointing to NULL 
+        // it is pointing to head
+
+        if(k %= len) 
+        {
+            for(auto i=0; i<len-k; i++) tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
+        }
+        newH = tail->next; // making new head 
+        tail->next = NULL; // pointing tail next elemet as NULL
+        return newH;
+    }
+};
